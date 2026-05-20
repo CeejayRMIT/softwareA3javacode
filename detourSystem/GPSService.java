4b01869f24e829345a4eda78a13a962fd76171c4
CeejayRMIT <s4170883@student.rmit.edu.au>
2026-05-20 19:15:19 +1000

    fix

--- detourSystem/GPSService.java
+++ detourSystem/GPSService.java
@@ -1,6 +1,6 @@
 public class GPSService {
-
     //start of loop
     public Location getCurrentLocation() {
-        return new Location(37.8136, 144.9631); //dummy coordinate data
+        return new Location(37.8136, 144.9631, 0.0);  // Placeholder
     }
+}
